2c4ee3a68db68a3c68a88e58e652170b49c82ae6
Paula Panijan <146742133+Paula-Panijan-tus22@users.noreply.github.com>
2025-04-01 12:22:31 +0100

    Hands Do stuff

--- Assets/CreatureFeatureAssets/Scripts/AnimationController.cs
+++ Assets/CreatureFeatureAssets/Scripts/AnimationController.cs
@@ -14,20 +14,32 @@ public class AnimationController : MonoBehaviour
     public void Dance()
     {
         animator.SetBool("isDancing", true);
+        animator.SetBool("isWaving", false);
+        animator.SetBool("isSitting", false);
+        animator.SetBool("isSpinning", false);
     }
 
     public void Wave()
     {
         animator.SetBool("isWaving", true);
+        animator.SetBool("isDancing", false);
+        animator.SetBool("isSitting", false);
+        animator.SetBool("isSpinning", false);
     }
 
     public void SitDown()
     {
         animator.SetBool("isSitting", true);
+        animator.SetBool("isDancing", false);
+        animator.SetBool("isWaving", false);
+        animator.SetBool("isSpinning", false);
     }
 
     public void Spin()
     {
+        animator.SetBool("isDancing", false);
         animator.SetBool("isSpinning", true);
+        animator.SetBool("isWaving", false);
+        animator.SetBool("isSitting", false);
     }
 }
