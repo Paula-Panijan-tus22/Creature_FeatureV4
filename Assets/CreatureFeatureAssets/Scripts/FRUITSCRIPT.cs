abded94da11e6479c6da6d42279a24ac595db0b6
Beanie <K00281273@student.tus.ie>
2025-04-29 18:24:46 +0100

    fruitAdded

---
+++ Assets/CreatureFeatureAssets/Scripts/FRUITSCRIPT.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FRUITSCRIPT : MonoBehaviour
+{
+    public bool isApple = false;
+    public bool isPear = false;
+    public bool isOrange = false;
+    public Transform spawnPoint;
+    public GameObject Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(Player.transform.position, transform.position) > 2)
+        {
+            Respawn();
+        }
+    }
+
+    public void Respawn()
+    {
+        transform.position = spawnPoint.position;
+        transform.rotation = spawnPoint.rotation;
+    }
+}
