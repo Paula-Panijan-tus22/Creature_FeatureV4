f9f1cc471df314e2e3af701c9ad922d4b30923a9
Beanie <K00281273@student.tus.ie>
2025-05-01 20:11:34 +0100

    fruit scripts

--- Assets/CreatureFeatureAssets/Scripts/eatFruitScript.cs
+++ Assets/CreatureFeatureAssets/Scripts/eatFruitScript.cs
@@ -7,24 +7,102 @@ public class eatFruitScript : MonoBehaviour
 {
     public UnityEvent happy;
     public UnityEvent sad;
+    public GameObject thoughtCloud;
+    public GameObject appleSprite;
+    public GameObject orangeSprite;
+    public GameObject pearSprite;
+    private float countdown_;
+    private bool thinkingOfFruit;
+    private int whichFruit;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        thoughtCloud.SetActive(false);
+        appleSprite.SetActive(false);
+        orangeSprite.SetActive(false);
+        pearSprite.SetActive(false);
+        countdown_ = 20;
+        thinkingOfFruit = false;
+    }
+
+    void Update()
+    {
+        countdown_ -= Time.deltaTime;
+        if (countdown_ > 0 && !thinkingOfFruit)
+        {
+            thinkOfFruit();
+        }
+
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.GetComponent<fruitDestroy>())
+        if (thinkingOfFruit)
         {
-            happy.Invoke();
-            collision = null;
-        } else
-        {
-            Debug.Log("not fruit");
+            if (collision.gameObject.GetComponent<FRUITSCRIPT>())
+            {
+                if (whichFruit == 1)
+                {
+                    if (collision.gameObject.GetComponent<FRUITSCRIPT>().isApple)
+                    {
+                        happy.Invoke();
+                    } else
+                    {
+                        sad.Invoke();
+                    }
+                }
+                else if (whichFruit == 2)
+                {
+                    if (collision.gameObject.GetComponent<FRUITSCRIPT>().isOrange)
+                    {
+                        happy.Invoke();
+                    }
+                    else
+                    {
+                        sad.Invoke();
+                    }
+                }
+                else if (whichFruit == 3)
+                {
+                    if (collision.gameObject.GetComponent<FRUITSCRIPT>().isPear)
+                    {
+                        happy.Invoke();
+                    }
+                    else
+                    {
+                        sad.Invoke();
+                    }
+                }
+                countdown_ = Random.Range(15,25);
+                thoughtCloud.SetActive(false);
+                appleSprite.SetActive(false);
+                orangeSprite.SetActive(false);
+                pearSprite.SetActive(false);
+                thinkingOfFruit = false;
+                collision.gameObject.GetComponent<FRUITSCRIPT>().Respawn();
+            }
+            else
+            {
+                Debug.Log("not fruit");
+            }
         }
     }
 
-    
+    private void thinkOfFruit()
+    {
+        thinkingOfFruit = true;
+        whichFruit = Random.Range(1, 3);
+        thoughtCloud.SetActive(true);
+        if (whichFruit == 1)
+        {
+            appleSprite.SetActive(true);
+        } else if (whichFruit == 2)
+        {
+            orangeSprite.SetActive(true);
+        } else if(whichFruit == 3)
+        {
+            pearSprite.SetActive(true);
+        }
+    }
 }
