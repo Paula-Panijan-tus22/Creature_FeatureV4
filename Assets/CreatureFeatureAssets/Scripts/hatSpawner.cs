e19272011faaeecde283e404599356fec550d220
Paula Panijan <146742133+Paula-Panijan-tus22@users.noreply.github.com>
2025-05-02 10:59:04 +0100

    Hats fixed

--- Assets/CreatureFeatureAssets/Scripts/hatSpawner.cs
+++ Assets/CreatureFeatureAssets/Scripts/hatSpawner.cs
@@ -6,15 +6,15 @@ public class hatSpawner : MonoBehaviour
 {
     public Transform spawnPoint;  // Assign the spawn location in the Inspector
   private GameObject spawnedObject;  // Stores reference to the spawned object
-  public Vector3 spawnOffset;
+
 
   // Function to spawn or destroy the object
   public void ToggleObject(GameObject objectPrefab)
     {
         if (spawnedObject == null)  // If no object exists, spawn it
         {
-      spawnedObject = Instantiate(objectPrefab, spawnPoint.position + spawnOffset, spawnPoint.rotation);
-      spawnedObject.transform.SetParent(spawnPoint);  // Parent it to the spawnPoint
+            spawnedObject = Instantiate(objectPrefab, spawnPoint.position, spawnPoint.rotation);
+            spawnedObject.transform.SetParent(spawnPoint);  // Parent it to the spawnPoint
         }
         else  // If object exists, destroy it
         {
